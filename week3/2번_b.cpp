b8366a0a59646b244ce642b3b7eafceac57bddb3
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-12 23:31:47 +0900

    Create 2번_b.cpp

---
+++ week3/2번_b.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Dice {
+private:
+	int face;
+public:
+	Dice() {
+		roll();
+	}
+	void roll() {
+		face = rand() % 6 + 1;
+	}
+	int getFace() {
+		return face;
+	}
+};
+int main() {
+	Dice d;
+	int count = 0;
+
+	for (int i = 0; i < 1000; i++) {
+		d.roll();
+		if (d.getFace() == 3) {
+			count++;
+		}	
+	}
+	cout << "테스트 확률: " << (double)count / 1000 << endl;
+	cout << "수학적 확률: " << 1.0 / 6.0 << endl;
+
+	return 0;
+}
