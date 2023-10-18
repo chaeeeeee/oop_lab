cbdee9ee26f0a58f7db4a8ed88361dc8600f032e
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-18 21:29:15 +0900

    Update 1번.cpp

--- week2/1번.cpp
+++ week2/1번.cpp
@@ -2,23 +2,18 @@
 #include <string>
 using namespace std;
 
-void swap(int x, int y) {
+void swap(int& x, int& y) {
 	int temp = x;
 	x = y;
 	y = temp;
 }
-void swap(int* x, int* y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
 int main() {
 	int a = 10, b = 20;
 
-	swap(a, b);
+	//swap(a, b);
 	cout << "a=" << a << "\tb=" << b << endl;
 
-	swap(&a, &b);
+	swap(a, b);
 	cout << "a=" << a << "\tb=" << b << endl;
 
 	return 0;
