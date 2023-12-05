7ced907149495a92b28ece6187f7553710fbbc52
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-12-05 15:37:43 +0900

    Creaate1번_b.cpp

--- week11/1번_b.cpp
+++ week11/1번_b.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
 class A {
 	int a;
 public:
-	A() { a = 0; cout << "생성자 A: " << a << endl; }
-	~A() { cout << "소멸자 A" << endl; }
+	A() { 
+		a = 0; 
+		cout << "생성자 A: " << a << endl; 
+	}
+	~A() { 
+		cout << "소멸자 A" << endl; 
+	}
 };
 class B : public A {
 	int b;
 public:
-	B() { b = 0; cout << "생성자 B: " << b << endl; }
-	~B() { cout << "소멸자 B" << endl; }
+	B() { 
+		b = 0; 
+		cout << "생성자 B: " << b << endl; 
+	}
+	~B() { 
+		cout << "소멸자 B" << endl; 
+	}
 };
 class C : public B {
 	int c;
 public:
-	C() { c = 0; cout << "생성자 C: " << c << endl; }
-	~C() { cout << "소멸자 C" << endl; }
+	C() { 
+		c = 0; 
+		cout << "생성자 C: " << c << endl; 
+	}
+	~C() { 
+		cout << "소멸자 C" << endl; 
+	}
 };
 int main() {
 	A a;
